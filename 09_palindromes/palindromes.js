82d1ccaff4b2d2865385c410b4ece95a25ae6041
Patrick Brebner <patrick.brebner@gmail.com>
2023-01-20 17:00:11 -0800

    Complete Exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
 
+    let newPhrase = phrase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s/g,"").toLowerCase();
+    let phraseLength = newPhrase.length;
+
+    if (phraseLength % 2 == 0 ) {
+        if (newPhrase.slice(0,phraseLength/2) == newPhrase.slice(phraseLength/2).split("").reverse().join("")) {
+            return true
+        } else return false
+    }
+    else {
+        if (newPhrase.slice(0, Math.ceil(phraseLength/2) - 1) == newPhrase.slice(Math.ceil(phraseLength/2)).split("").reverse().join("")) {
+            return true
+        } else return false
+    }
+    
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+
+    if (index < 1) {
+        return "OOPS"
+    }
+
+    let fibArray = [1,1]
+    for (let i = 2; i <= index; i++) {
+        fibArray[i] = fibArray[i-1] + fibArray[i-2]
+    }
+    //console.table(fibArray)
+
+    return fibArray[index-1]
 
 };
 
